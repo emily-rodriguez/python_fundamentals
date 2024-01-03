4bc9a73182ef8a462600f7f3b736db4e83ed3dde
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2024-01-03 14:11:21 -0600

    working

--- convert_files.py
+++ convert_files.py
@@ -1,85 +1,57 @@
 import os
 from camel_converter import to_snake
-import shutil
-import fileinput
 
 
 olddir = 'old'
 newdir = 'cleaned'
 
-#copy and rename all files
 
 def is_camel_case(s):
-  if s != s.lower() and s != s.upper() and "_" not in s and sum(i.isupper() for i in s[1:-1]) == 1:
-      return True
-  return False
+    return s != s.lower() and s != s.upper() and "_" not in s and "-" not in s
 
 
-def contains_camel_case(line):
-    for s in line.split():
-        return is_camel_case(s)
+def handle_id(s):
+    print(f'STRING: {s}')
+    if "ID" in s:
+        return s.replace("ID", "Id")
+    else:
+        return s
 
+def handle_special_cases(str):
+    if is_camel_case(str):
+        return to_snake(str)
+    elif str[-1] == '_':
+        split_str = str.split('_')
+        return to_snake(split_str[0]) + '_'
+    elif str[0] == '_':
+        split_str = str.split('_')
+        return '_' + to_snake(split_str[1])
+    elif str[0] == 'pii_':
+        split_str = str.split('_')
+        return 'pii_' + to_snake(split_str[1])
+    else:
+        return str
 
-def find_camel_case(line):
-    for s in line.split():
-        if is_camel_case(s):
-            return s
-
-
-def fix_line(line):
-    camel_text = find_camel_case(line)
-    print(f'Found camelcase! {camel_text}')
-    new_text = to_snake(camel_text)
-    return line.replace(camel_text, new_text)
-
-
-
-for root, dirs, files in os.walk(olddir):
-    for filename in files:
-        old_name = os.path.join( os.path.abspath(root), filename )
-
-        print(f'Copying {old_name}...')
-
-        base, extension = os.path.splitext(filename)
-
-        new_name = os.path.join(newdir, to_snake(filename))
-
-        if not os.path.exists(new_name):
-            shutil.copy(old_name, new_name)
-            print(f'Copied {old_name} as {new_name}')
-
-
-#replace all camelCase in copied files
 
 for root, dirs, files in os.walk(olddir):
     for filename in files:
         print(f'Working on {filename}')
 
-        old_file = os.path.join( os.path.abspath(root), filename )
+        old_file = os.path.join(os.path.abspath(root), filename)
 
         print(f'Copying {old_file}...')
 
         base, extension = os.path.splitext(filename)
         new_name = to_snake(filename)
-        print(f'New file name: {new_name}')
         new_file = open(os.path.join(newdir, new_name), 'w')
 
-        with open(old_file, 'r') as f:
+        with open(old_file, 'r+') as f:
             contents = f.readlines()
 
         for line in contents:
-            # print(f'Line: {line}')
-            if contains_camel_case(line):
-                for s in line.split():
-                    if is_camel_case(s):
-                        line = line.replace(s, to_snake(s))
-                        print(f'FIXED LINE: {line}')
-                    print(f'Line 77 {line}')
-
-
-            else:
-                new_file.write(line)
-
-
-
-
+            for s in line.split():
+                edited = handle_id(s)
+                replaced = handle_special_cases(edited)
+                line = line.replace(s, replaced)
+            print(f'Replaced Line: {line}')
+            new_file.write(line)
