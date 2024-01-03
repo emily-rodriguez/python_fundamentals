4e437626e6ca9c3116bb924438181d20eb1acd34
emily-rodriguez <emily@jobward.com>
2024-01-03 07:34:02 -0600

    file converter

--- convert_files.py
+++ convert_files.py
@@ -12,15 +12,19 @@ for root, dirs, files in os.walk(olddir):
     for filename in files:
         old_name = os.path.join( os.path.abspath(root), filename )
 
-        print(f'root: {os.path.abspath(root)}')
         print(f'Copying {old_name}...')
 
         base, extension = os.path.splitext(filename)
 
         new_name = os.path.join(newdir, to_snake(filename))
 
-        print(f'New Name: {new_name}')
-
         if not os.path.exists(new_name):
             shutil.copy(old_name, new_name)
             print(f'Copied {old_name} as {new_name}')
+
+
+#replace all camelCase in copied files
+
+for root, dirs, files in os.walk(newdir):
+    for filename in files:
+        print(f'Working on {filename}')
